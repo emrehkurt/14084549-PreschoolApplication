ed0747dc5b3b66ca950384f5457267c86a07c829
ek12aaa <ek12aaa@cl-2e105.herts.ac.uk>
2017-03-21 19:20:11 +0000

    Initial Commit

---
+++ PreschoolApplication-14084549/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PreschoolApplication-14084549
+//
+//  Created by ek12aaa on 21/03/2017.
+//  Copyright © 2017 ek12aaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
