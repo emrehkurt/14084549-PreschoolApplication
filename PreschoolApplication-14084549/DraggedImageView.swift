24e250d0928c5d049f3d34897bd2ab1ef4f53757
ek12aaa <ek12aaa@CL-2E117.herts.ac.uk>
2017-03-22 19:01:42 +0000

    first commit

---
+++ PreschoolApplication-14084549/DraggedImageView.swift
@@ -0,0 +1,13 @@
+//
+//  DraggedImageView.swift
+//  PreschoolApplication-14084549
+//
+//  Created by ek12aaa on 21/03/2017.
+//  Copyright © 2017 ek12aaa. All rights reserved.
+//
+
+import Cocoa
+
+class DraggedImageView: UIImageView {
+
+}
